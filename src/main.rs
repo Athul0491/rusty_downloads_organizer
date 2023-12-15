92164c821c00616b3c5f0eb37a8c9758a305ae6f
Athul0491 <65664185+Athul0491@users.noreply.github.com>
2023-12-15 17:18:46 +0530

    feat: added support for video files

--- src/main.rs
+++ src/main.rs
@@ -35,12 +35,15 @@ fn make_unique(dest: &str, name: &str) -> String {
 fn organise_files(source: &str) -> io::Result<()> {
     let dest_dir_image: &str = "D:\\Downloads\\Image";
     let dest_dir_audio: &str = "D:\\Downloads\\Audio";
+    let dest_dir_video: &str = "D:\\Downloads\\Video";
 
     let image_extensions: [&str;37] = ["jpg", "jpeg", "jpe", "jif", "jfif", "jfi", "png", "gif", "webp", "tiff",
                     "tif", "psd", "raw", "arw", "cr2", "nrw", "k25", "bmp", "dib", "heif", "heic",
                     "ind", "indd", "indt", "jp2", "j2k", "jpf", "jpf", "jpx", "jpm", "mj2", "svg",
                     "svgz", "ai", "eps", "ico", "avif"];
     let audio_extensions: [&str;6] = [".m4a", ".flac", "mp3", ".wav", ".wma", ".aac"];
+    let video_extensions: [&str;17] = [".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg",".mp4", ".mp4v", ".m4v", ".avi", 
+                    ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd"];
 
     // Use read_dir to get an iterator of DirEntry objects
     let entries = fs::read_dir(source)?;
@@ -59,6 +62,12 @@ fn organise_files(source: &str) -> io::Result<()> {
                 }
                 if audio_extensions.contains(&extension.to_str().unwrap()) {
                     let unique_dest: String = make_unique(dest_dir_audio, entry.file_name().to_str().unwrap());
+                    let destination_path: &Path = Path::new(&unique_dest);// Use Path::new and join for path manipulation
+                    fs::rename(entry.path(), &destination_path)?; // Use rename for moving files
+                    println!("Moved {} to {}", entry.path().display(), destination_path.display());
+                }
+                if video_extensions.contains(&extension.to_str().unwrap()) {
+                    let unique_dest: String = make_unique(dest_dir_video, entry.file_name().to_str().unwrap());
                     let destination_path: &Path = Path::new(&unique_dest);// Use Path::new and join for path manipulation
                     fs::rename(entry.path(), &destination_path)?; // Use rename for moving files
                     println!("Moved {} to {}", entry.path().display(), destination_path.display());
